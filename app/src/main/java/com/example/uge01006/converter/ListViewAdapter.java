1385666e98f304da44b5528983e9a64d41d84c0b
srua <starfly1570@gmail.com>
2017-09-08 15:53:22 +0200

    08/09/2017
    Noticed github takes my commits as "espy11" the one doing this project is me alone anyways.
    
    Added colour palette.
    Added menu.
    Added test items.

--- app/src/main/java/com/example/uge01006/converter/ListViewAdapter.java
+++ app/src/main/java/com/example/uge01006/converter/ListViewAdapter.java
@@ -7,7 +7,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.MediaController;
+import android.widget.TextView;
 import android.widget.VideoView;
 import java.util.List;
 public class ListViewAdapter extends ArrayAdapter<Result>
@@ -28,18 +30,23 @@ public class ListViewAdapter extends ArrayAdapter<Result>
             LayoutInflater inflater = LayoutInflater.from(getContext());
             convertView = inflater.inflate(R.layout.listview_layout, parent, false);
         }
+
         VideoView video = convertView.findViewById(R.id.VVvideo);
+        TextView title = convertView.findViewById(R.id.TVtitle);
+        TextView length = convertView.findViewById(R.id.TVlength);
 
-        /** Meter el video en el VideoView a partir de la información del item **/
+        title.setText(item.getTitle());
+        length.setText(item.getLength());
 
 
-
+        /** Meter el video en el VideoView a partir de la información del item
         video.setVideoURI(Uri.parse("https://www.youtube.com/watch?v=JsKIAO11q1Y&list=FLtYx3YJq_0mlhJO_rpE1MTQ"));
       //  video.setVideoURI(Uri.parse("rtsp://v4.cache3.c.youtube.com/CjYLENy73wIaLQlW_ji2apr6AxMYDSANFEIJbXYtZ29vZ2xlSARSBXdhdGNoYOr_86Xm06e5UAw=/0/0/0/video.3gp"));
         video.setMediaController(new MediaController(getContext())); //sets MediaController in the video view
         // MediaController containing controls for a MediaPlayer
         video.requestFocus();//give focus to a specific view
         video.start();//starts the video
+         */
 
         return convertView;
     }
--- app/src/main/java/com/example/uge01006/converter/MainActivity.java
+++ app/src/main/java/com/example/uge01006/converter/MainActivity.java
@@ -2,13 +2,15 @@ package com.example.uge01006.converter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
 {
-    private ArrayList<Result> results = new ArrayList<>();
+    private ArrayList<Result> items;
     private ListViewAdapter listViewAdapter;
     private ListView listView;
 
@@ -17,9 +19,41 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listViewAdapter = new ListViewAdapter(this, 0, results);
+        items = new ArrayList<>();
+        listViewAdapter = new ListViewAdapter(this, 0, items);
         listView = (ListView) this.findViewById(R.id.LVitems);
         listView.setAdapter(listViewAdapter);
-
+        testItems();
     }
+
+    public void testItems()
+    {
+        Result a = new Result();
+        a.setTitle("David Guetta ft. Justin Bieber - 2U");
+        a.setLength("3:27 minutes");
+        items.add(a);
+
+        Result b = new Result();
+        b.setTitle("Lost Frequencies & Netsky - Here With You");
+        b.setLength("Length 2:53");
+        items.add(a);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+        if (id == R.id.action_search) {return true;}
+
+        if (id == R.id.action_settings) {return true;}
+        return super.onOptionsItemSelected(item);
+    }
+
 }
