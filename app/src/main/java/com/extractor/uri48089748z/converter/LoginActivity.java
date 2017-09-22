dff6e481fa3ef4666718c5097fd1df3ca792a530
srua <starfly1570@gmail.com>
2017-09-22 17:55:51 +0200

    Implementing facebook login..

--- app/src/main/java/com/extractor/uri48089748z/converter/LoginActivity.java
+++ app/src/main/java/com/extractor/uri48089748z/converter/LoginActivity.java
@@ -4,20 +4,35 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
+public class LoginActivity extends AppCompatActivity
+{
+    private CallbackManager callbackManager;
+    private Intent main;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_login);
+        AppEventsLogger.activateApp(this);
 
-        try {Thread.sleep(3000);}
-        catch (InterruptedException e) {e.printStackTrace();}
-        finally
-        {
-            Intent main = new Intent(this, MainActivity.class);
-            startActivity(main);
-        }
+        main = new Intent(this, MainActivity.class);
+        callbackManager = CallbackManager.Factory.create();
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>()
+                {
+                    @Override public void onSuccess(LoginResult loginResult) {startActivity(main);}
+                    @Override public void onCancel() {}
+                    @Override public void onError(FacebookException exception) {}
+                });
     }
 }
